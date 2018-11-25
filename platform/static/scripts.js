b63fde1fefc70422085c3aa1cec781825238f6c5
nQuery512 <nQuery512@gmail.com>
2018-11-25 19:32:52 +0100

    Add files via upload

--- platform/static/scripts.js
+++ platform/static/scripts.js
@@ -7,15 +7,18 @@ function getSelectedValue()
 	var station_array = Array();
 	var url = "http://localhost:5000/stations?name="+text;
 
+	// Activating spinner on button click
+	$('#spinner').css('display', 'inline-block');
+
 	$.get(url, function( data ) {
+		// Remove spinner when response come back from server
+		$('#spinner').css('display', 'none');
+
   		$( ".result" ).html( data );
 		//console.log(decodedString);
 		my_array = JSON.parse(data);
 		my_array.forEach(function(obj)
     	{
-	    	//console.log(typeof(obj));
-	    	//
-	    	//console.log(obj['station']['code']);
 	    	if(obj['station']['name'] == text)
 	    	{
 	    		console.log(obj);
@@ -26,12 +29,26 @@ function getSelectedValue()
 	    if(station_array['station']['state'] == "Operative")
 	    {
 	    	$("#station-activity").css('background-color', "#00FF00");
-	    	var dataa = [station_array['nbBike'], station_array['nbEbike']];
+	    	$('#station-state').css('display', 'block');
+	    	$('#station-overflow').css('display', 'block');
+	    	
+
+	    	// Vélo meca + vélo meca overflow ET velo elec + vélo elec overflow
+	    	if(station_array['overflow'] == "yes")
+	    	{
+				$('#station-overflow').html("Surplus de vélo: Oui");
+				var dataa = [station_array['nbBike']+station_array['nbBikeOverflow'], station_array['nbEbike']+station_array['nbEBikeOverflow']];
+	    	}
+	    	else
+	    	{
+	    		$('#station-overflow').html("Surplus de vélo: Non");
+	    		var dataa = [station_array['nbBike'], station_array['nbEbike']];
+	    	}
+
 	    	var background_color = [
                	"#34bf36",
                	"#28bf96"
-            ]; 
-	    	alert(dataa);
+            ];  
 
 	    	var datas = {
 				datasets : [{ 
@@ -42,11 +59,24 @@ function getSelectedValue()
 	    		labels : ['Vélo mécanique disponible', 'Vélo électrique disponible'],
 	    	};
 	    	
-	    	var optionss =  {
+	    	var options_dispo =  {
+	    		cutoutPercentage: 60,	
 	    		responsive: true,
       			title: {
 					display: true,
-					text: 'Disponibilité'
+					text: ["Station: "+station_array['station']['name'],"Disponibilité d'emprunts en temps réels","Overflow inclus"],
+					fontSize: 20
+				},
+				maintainAspectRatio : false
+    		};
+
+    		var options_retour =  {
+	    		cutoutPercentage: 60,	
+	    		responsive: true,
+      			title: {
+					display: true,
+					text: ["Station: "+station_array['station']['name'],"Disponibilité de retour en temps réels"],
+					fontSize: 20
 				},
 				maintainAspectRatio : false
     		};
@@ -56,8 +86,16 @@ function getSelectedValue()
 	    	var myDoughnutChart = new Chart(ctx, {
     			type: 'doughnut',
     			data: datas,
-    			options: optionss
+    			options: options_dispo
 			});	
+
+			/*var ctx = document.getElementById("retour-graph").getContext('2d');
+
+	    	var myDoughnutChart = new Chart(ctx, {
+    			type: 'doughnut',
+    			data: datas,
+    			options: options_retour
+			});*/	
 		}
 
 	    else
@@ -66,5 +104,7 @@ function getSelectedValue()
 	    	alert("Cette station est fermée pour le moment.");
 	    }
 	});
+
+
 	
 }
